32ebfda506e90a9765451f1d0db035b1872277e9
angelo sacchiero <angelo.sacchiero@gmail.com>
2025-05-29 11:42:09 +0200

    riordinata classe Temi

--- Campo_minato3/CTema.cs
+++ Campo_minato3/CTema.cs
@@ -10,24 +10,37 @@ namespace Campo_minato3
 {
     public class CTema
     {
-        Color[] defaultC = new Color[]
-        {
-            Color.FromArgb(0, 0, 255),       // 1 - Blu
-            Color.FromArgb(0, 128, 0),       // 2 - Verde
-            Color.FromArgb(255, 0, 0),       // 3 - Rosso
-            Color.FromArgb(0, 0, 139),       // 4 - Blu scuro
-            Color.FromArgb(139, 69, 19),     // 5 - Marrone scuro
-            Color.FromArgb(72, 209, 204),    // 6 - Ciano scuro
-            Color.FromArgb(0, 0, 0),         // 7 - Nero
-            Color.FromArgb(105, 105, 105)    // 8 - Grigio scuro
-        };
-
-        
+        public string nomeTema { get; set; }
         public Font font { get; set; }
         public Color[] colori { get; set; }
         public string bomba { get; set; }
         public string bandiera { get; set; }
         public SoundPlayer[] Suoni { get; set; }
+
+        public CTema()
+        {
+            nomeTema = "Default";
+            font = new Font(FontFamily.GenericSansSerif, 14.25f, FontStyle.Regular);
+
+            colori = new Color[]
+            {
+                ColorTranslator.FromHtml("#0000FF"), // 1 - blu
+                ColorTranslator.FromHtml("#008000"), // 2 - verde
+                ColorTranslator.FromHtml("#FF0000"), // 3 - rosso
+                ColorTranslator.FromHtml("#00008B"), // 4 - blu scuro
+                ColorTranslator.FromHtml("#8B4513"), // 5 - marrone scuro
+                ColorTranslator.FromHtml("#48D1CC"), // 6 - ciano scuro
+                ColorTranslator.FromHtml("#000000"), // 7 - nero
+                ColorTranslator.FromHtml("#696969"), // 8 - grigio scuro
+                ColorTranslator.FromHtml("#AAAAAA"), // cella non scoperta
+                ColorTranslator.FromHtml("#F0F0F0"), // cella scoperta
+                ColorTranslator.FromHtml("#FF1428"), // cella perdita
+                ColorTranslator.FromHtml("#000000")  // colore default
+            };
+
+            bomba = "💣";
+            bandiera = "🚩";
+        }
 
         public CTema(Font font, Color[] colori, string bomba, string bandiera, SoundPlayer[] suoni)
         {
@@ -36,14 +49,6 @@ namespace Campo_minato3
             this.bomba = bomba;
             this.bandiera = bandiera;
             this.Suoni = suoni;
-        }
-
-        public CTema()
-        {
-            font = new Font(FontFamily.GenericSansSerif,14.25f, FontStyle.Regular);
-            colori = defaultC;
-            bomba = "💣";
-            bandiera = "🚩";
         }
     }
 }
